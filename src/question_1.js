48ff20fbe0598fb31da270b05709b32be450e266
Thiago Adriano <thiago.s.adriano@gmail.com>
2019-11-02 15:42:56 -0300

    Feito questão 1

--- src/question_1.js
+++ src/question_1.js
@@ -1,6 +1,30 @@
-function Main(input = null) {
+function isValidAlvo(alvo) {
+  return !Number.isNaN(alvo) && alvo > 0;
+}
 
-  return input;
+function isValidList(list) {
+  return Array.isArray(list) && list.length;
+}
+
+function calculate(alvo, list) {
+  let indices = [];
+  let total = list.length;
+
+  for (let i = 0; i < total; i++) {
+    let calc = list[i] + list[i + 1];
+    if (!Number.isNaN(calc) && calc === alvo) {
+      indices.push(i, i + 1);
+    }
+  }
+
+  return indices;
+}
+
+function Main(alvo = 0, list = []) {
+  if(!isValidAlvo(alvo) || !isValidList(list)) {
+    return false;
+  }
+  return calculate(alvo, list);
 }
 
 module.exports = Main;
--- test/question_1.spec.js
+++ test/question_1.spec.js
@@ -1,8 +1,41 @@
 const { expect } = require('chai');
 const question = require('./../src/question_1');
 
-describe('Questão 1', () => {
- it('Deve garantir a chamada da função', () => {
-   expect(question()).to.null;
+describe('Questão 1 - validações', () => {
+  it('Deve garantir a chamada da função sem valores retorne falso', () => {
+    expect(question()).to.false;
+  });
+
+  it('Deve garantir que o alvo inválido não tenha o cálculo', () =>{
+    expect(question('valor', [1,2,3])).to.false;
+    expect(question(0, [1,2,3])).to.false;
+    expect(question([], [1,2,3])).to.false;
  });
+
+  it('Deve garantir que a lista inválida não tenha o cálculo', () =>{
+    expect(question(3, [])).to.false;
+    expect(question(3, null)).to.false;
+    expect(question(3, 'valor')).to.false;
+  });
+});
+
+
+describe('Questão 1 - teste valores', () => {
+  it('Deve garantir um array vazio para o alvo 3', () => {
+    let ints = [10, 23, 52, 45, 83];
+    let alvo = 3;
+    expect(question(alvo, ints)).to.eql([]);
+  });
+
+  it('Deve garantir um array com os indices 3 e 4 para o alvo 8', () => {
+     let ints = [0, 3, 2, 5, 3, 8, 5];
+     let alvo = 8;
+     expect(question(alvo, ints)).eql([3, 4]);
+  });
+
+  it('Deve garantir um array com os indices 2,3, 6,7 e 10,11 para o alvo 15', () => {
+    let ints = [0,'15', 5,10, 8,25, 14,1, 25,30, 7,8];
+    let alvo = 15;
+    expect(question(alvo, ints)).eql([2,3, 6,7, 10,11]);
+  });
 });
