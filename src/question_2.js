272ea2205d56ad182581e8a4d3bd2006113d2332
Thiago Adriano <thiago.s.adriano@gmail.com>
2019-11-02 23:42:52 -0300

    Resolvido a questão 2

--- src/question_2.js
+++ src/question_2.js
@@ -1,6 +1,47 @@
-function Main(input = null) {
+function isValidInput(input) {
+  let pattern = /[^\[\]\(\)\{\}]/g;
+  return typeof input === 'string' &&
+         input.length > 0  &&
+         input.length % 2 === 0 &&
+         !pattern.test(input);
+}
 
-  return input;
+function caseCloseBrackets(bracketOpenString) {
+  switch(bracketOpenString) {
+    case '{':
+      return '}';
+    case '[':
+      return ']';
+    case '(':
+      return ')';
+    default:
+      return '';
+  }
+}
+
+function testValue(input) {
+  let totalString = input.length;
+  let run = totalString / 2;
+
+  for (let i = 0; i < run; i++) {
+    let first = input[i];
+    let last = input[totalString - (i + 1)];
+    let closeBracket = caseCloseBrackets(first);
+
+    if (last !== closeBracket) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function Main(input = '') {
+  if(!isValidInput(input)) {
+    return 'String Inválida';
+  }
+
+  return testValue(input) ? 'SIM' : 'NAO';
 }
 
 module.exports = Main;
--- test/question_2.spec.js
+++ test/question_2.spec.js
@@ -1,8 +1,50 @@
 const { expect } = require('chai');
 const question = require('./../src/question_2');
 
-describe('Questão 2', () => {
+describe('Questão 2 - validações', () => {
   it('Deve garantir a chamada da função', () => {
-    expect(question()).to.null;
+    expect(question()).to.equal('String Inválida');
+  });
+
+  it('Deve garantir que o parametro é uma string', () => {
+    expect(question([])).to.equal('String Inválida');
+    expect(question(2)).to.equal('String Inválida');
+    expect(question(null)).to.equal('String Inválida');
+    expect(question(undefined)).to.equal('String Inválida');
+  });
+
+  it('Deve garantir que NAO executa com uma string válida', () => {
+    expect(question('(sdqe)')).to.equal('String Inválida');
+  });
+});
+
+describe('Questão 2 - teste de valores', () => {
+  it('Deve garantir que funcione com somente um bracket (), [] ou {} e retorne SIM', () => {
+    expect(question('()')).to.equal('SIM');
+    expect(question('[]')).to.equal('SIM');
+    expect(question('{}')).to.equal('SIM');
+  });
+  it('Deve garantir que funcione com dois brackets ({}), [()] ou {[]} e retorne SIM', () => {
+    expect(question('({})')).to.equal('SIM');
+    expect(question('[()]')).to.equal('SIM');
+    expect(question('{[]}')).to.equal('SIM');
+    expect(question('{()}')).to.equal('SIM');
+  });
+  it('Deve garantir que funcione com três brackets ({[]}), [({})] ou {[()]} e retorne SIM', () => {
+    expect(question('({[]})')).to.equal('SIM');
+    expect(question('[({})]')).to.equal('SIM');
+    expect(question('{[()]}')).to.equal('SIM');
+  });
+  it('Deve garantir que funcione com vários brackets e retorne SIM', () => {
+    expect(question('{{[[(())]]}}')).to.equal('SIM');
+  });
+  it('Deve garantir que funcione com um bracket (} e retorne NAO', () => {
+    expect(question('(}')).to.equal('NAO');
+  });
+  it('Deve garantir que funcione com dois brackets ({)} e retorne NAO', () => {
+    expect(question('({)}')).to.equal('NAO');
+  });
+  it('Deve garantir que funcione com três brackets {[(])} e retorne NAO', () => {
+    expect(question('{[(])}')).to.equal('NAO');
   });
 });
