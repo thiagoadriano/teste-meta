ab4ed783788c2a0045c3dfa2a8f4a46e7413b1ee
Thiago Adriano <thiago.s.adriano@gmail.com>
2019-11-04 02:06:25 -0200

    Finalizado questão 4

--- src/question_4.js
+++ src/question_4.js
@@ -1,6 +1,32 @@
-function Main(input = null) {
+function isValidList(list) {
+  if (Array.isArray(list)) {
+    let invalid = list.some(x => !Number.isInteger(x));
+    return list.length && !invalid;
+  }
+  return false;
+}
 
-  return input;
+function calculate(list) {
+  let returnedCalc = 0;
+  let total = list.length;
+
+  for(let i = 0; i < total; i++) {
+    let previous = list[i - 1],
+        current = list[i];
+    if (previous > current) {
+      returnedCalc += previous - current;
+    }
+  }
+
+  return returnedCalc;
+}
+
+function Main(list = []) {
+  if(!isValidList(list)) {
+    return false;
+  }
+
+  return calculate(list);
 }
 
 module.exports = Main;
--- test/question_4.spec.js
+++ test/question_4.spec.js
@@ -1,8 +1,39 @@
 const { expect } = require('chai');
 const question = require('./../src/question_4');
 
-describe('Questão 4', () => {
-  it('Deve garantir a chamada da função', () => {
-    expect(question()).to.null;
+describe('Questão 4 - validações', () => {
+  it('Deve garantir a chamada da função sem valores retorne falso', () => {
+    expect(question()).to.false;
   });
+
+  it('Deve garantir que não vai ser feito cálculo com valor diferente de número', () =>{
+    expect(question(['1',2,3])).to.false;
+    expect(question([])).to.false;
+    expect(question([2, {}, '3'])).to.false;
+ });
 });
+
+describe('Questão 4 - teste valores', () => {
+  it('Deve garantir que retorne 1 para [0,1,0,1]', () => {
+    expect(question([0,1,0,1])).to.equal(1);
+  });
+
+  it('Deve garantir que retorne 5 para [0,5,0,5]', () => {
+    expect(question([0,5,0,5])).to.equal(5);
+  });
+
+  it('Deve garantir que retorne 6 para [0,1,0,2,1,0,1,3,2,1,2,1]', () => {
+    expect(question([0,1,0,2,1,0,1,3,2,1,2,1])).to.equal(6);
+  });
+
+  it('Deve garantir que retorne 9 para [0,1,0,3,1,0,1,4,2,1,3,1]', () => {
+    expect(question([0,1,0,3,1,0,1,4,2,1,3,1])).to.equal(9);
+  });
+
+  it('Deve garantir que retorne 8 para [1,1,1,3,1,0,1,4,2,1,3,1]', () => {
+    expect(question([1,1,1,3,1,0,1,4,2,1,3,1])).to.equal(8);
+  });
+
+
+});
+
